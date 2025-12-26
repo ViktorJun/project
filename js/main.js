fab41e706cf3568e1f8cdacb17e83403671514b5
ViktorJun <viktorbylash@gmail.com>
2025-12-27 00:06:23 +0200

    fix

---
+++ js/main.js
@@ -0,0 +1,4 @@
+import {createMockData} from "./mockData.js";
+
+const mockData = createMockData();
+console.log(mockData)
--- js/mockData.js
+++ js/mockData.js
@@ -48,4 +48,3 @@ function createRandomMessage() {
     }
 }
 
-console.log(createMockData())
